1f7a503262be6aa6ef7ef1e47be57db8213bff8d
AlouiOmar97 <omar.aloui@esprit.tn>
2025-03-07 10:01:48 +0100

    atelier 1

--- src/app/residence/residence.component.ts
+++ src/app/residence/residence.component.ts
@@ -8,9 +8,26 @@ import { Residence } from '../core/models/residence';
 })
 export class ResidenceComponent {
   listResidences:Residence[]=[
-    {id:1,"name": "El fel","address":"Borj Cedria","image":"../../assets/images/R1.jpg", status: "Disponible"},
-    {id:2,"name": "El yasmine","address":"Ezzahra","image":"../../assets/images/R2.jpg", status: "Disponible" },
-    {id:3,"name": "El Arij", "address":"Rades","image":"../../assets/images/R3.jpg", status:"Vendu"},
-    {id:4,"name": "El Anber","address":"inconnu", "image":"../../assets/images/R4.jpg", status: "En Construction"}
+    {id:1,"name": "El fel","address":"Borj Cedria","image":"../../assets/images/R1.jpg", status: "Disponible", showLocation: false },
+    {id:2,"name": "El yasmine","address":"Ezzahra","image":"../../assets/images/R2.jpg", status: "Disponible" , showLocation: false },
+    {id:3,"name": "El Arij", "address":"Rades","image":"../../assets/images/R3.jpg", status:"Vendu", showLocation: false },
+    {id:4,"name": "El Anber","address":"inconnu", "image":"../../assets/images/R4.jpg", status: "En Construction", showLocation: false}
     ];
+  listFavoris:Residence[]=[]
+  search=""
+    toggleLocation(id: number){
+      for (let i = 0; i < this.listResidences.length; i++) {
+        if(this.listResidences[i].id === id){
+          if(this.listResidences[i].address === "inconnu"){
+            alert("Unknown Address")
+          }else{
+            this.listResidences[i].showLocation=!this.listResidences[i].showLocation
+          }
+        }
+      }
+    }
+
+    likeResidence(residence: Residence){
+      this.listFavoris.push(residence)
+    }
 }
